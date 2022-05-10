72864ff75fc0076c6ef3eb19f9031b0d136a7e0d
Affan <57275933+afruse@users.noreply.github.com>
2022-05-10 14:05:41 -0400

    Add files via upload

--- Insect.java
+++ Insect.java
@@ -15,5 +15,6 @@ public class Insect extends Actor
     public void act()
     {
         // Add your action code here.
+        move(1);
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -13,25 +13,28 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    private int score = 0;
     
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Snake s = new Snake();
+        border b = new border();
+        border b2 = new border();
+        border b3 = new border();
+        border b4 = new border();
+        border b5 = new border();
         addObject(s, 300, 200);
+        addObject(b, 600, 200);
+        addObject(b2, 600, 300);
+        addObject(b3, 600, 400);
+        addObject(b4, 600, 100);
+        addObject(b5, 600, 0);
         spawnInsect();
     }
     
     public void spawnInsect(){
         Insect i = new Insect();
         addObject(i, Greenfoot.getRandomNumber(600), Greenfoot.getRandomNumber(400));
-        score++;
-        System.out.println("You got a point!: " + score);
-    }
-    
-    public void checkDie(){
-        
     }
 }
